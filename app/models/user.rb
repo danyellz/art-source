ef6c4ee9e4292165d680afebf7e6d3304ff12e49
Ty Daniels <ty.multisonus@gmail.com>
2016-10-17 16:07:16 -0700

    Saves CSS layout for overall site

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+	has_secure_password
 	has_many :friends
+
+	attr_accessible :email, :secure_pass
+
+	validates_uniqueness_of :email, :on => :create, :message => "Email must be unique!"
 end
